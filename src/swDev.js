6d4564f6c9fbd4e11f1eaf9d4bb5a894265a3f77
unknown <bskrsingh@gmail.com>
2024-05-17 18:03:16 +0530

    first commit

--- src/swDev.js
+++ src/swDev.js
@@ -1,4 +1,5 @@
 export default function swDev(){
+    debugger
     let swUrl = `${process.env.PUBLIC_URL}/sw.js`
     navigator.serviceWorker.register(swUrl).then((res)=>{
         console.log('res',res);
