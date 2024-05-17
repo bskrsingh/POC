b2a873254434444e3dd7e1f337614156f604a8b8
unknown <bskrsingh@gmail.com>
2024-05-17 15:46:20 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,27 @@
+let cacheData = 'appV1';
+this.addEventListener('install',(event)=>{
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=>{
+            cache.addAll([
+                '/static/js/bundle.js',
+                '/index.html',
+                '/',
+                '/book-store',
+                'bookDetails',
+                'bookListing',
+            ])
+        })
+    )
+})
+
+this.addEventListener('fetch',(event)=>{
+    if(!navigator.onLine){
+        event.respondWith(
+            caches.match(event.request).then((resp)=>{
+                if(resp){
+                    return resp
+                }
+            })
+        )
+    }
+})
